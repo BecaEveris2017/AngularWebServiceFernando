6ca8d3792c4f1f22333faaf38b90c18d75fb07da
LuisFernandoMolina <luisfernandomolinay@outlook.com>
2017-10-28 20:55:15 -0500

    Todos los componentes

--- WebServiceFernando/src/app/app.component.ts
+++ WebServiceFernando/src/app/app.component.ts
@@ -1,15 +1,20 @@
 import { Component } from '@angular/core';
+import { PostService} from './post.service';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  
 })
 export class AppComponent {
   title = 'Beca everis 2017';
   colores=['rojo','negro','azul'];
   mostrarlistacolores=true;
-
+  nombre:string;
+  email:string;
+  
   addcolor(color){
     this.colores.push(color.value);
     color.value="";
@@ -18,5 +23,4 @@ export class AppComponent {
   mostrarcolores(){
     this.mostrarlistacolores=!this.mostrarlistacolores;
   }
-
 }
